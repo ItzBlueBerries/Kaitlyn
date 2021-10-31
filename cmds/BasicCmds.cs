906885d52585cc53081f2d553053cf13acc129b2
Jay M <fruitsyberries@gmail.com>
2021-10-31 08:19:05 -0400

    Typing + Ping CMD

--- cmds/BasicCmds.cs
+++ cmds/BasicCmds.cs
@@ -1,4 +1,5 @@
-﻿using DSharpPlus.CommandsNext;
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using System;
@@ -16,6 +17,7 @@ namespace Kaitlyn.cmds
         [Description("Kaitlyn says Hi! <3")]
         public async Task Hello(CommandContext ctx)
         {
+            await ctx.TriggerTypingAsync();
             await ctx.Channel.SendMessageAsync("Hello there! I'm Kaitlyn. <3\nYou can find my commands using `k!help`.").ConfigureAwait(false);
         }
 
@@ -26,6 +28,7 @@ namespace Kaitlyn.cmds
             [Description("Num. 1")] int one, 
             [Description("Num. 2")] int two)
         {
+            await ctx.TriggerTypingAsync();
             await ctx.Channel.SendMessageAsync((one + two)
                 .ToString())
                 .ConfigureAwait(false);
@@ -36,8 +39,17 @@ namespace Kaitlyn.cmds
         [Description("Get a users name!")]
         public async Task Getname(CommandContext ctx, [Description("Member Input")] DiscordMember user)
         {
+            await ctx.TriggerTypingAsync();
             string displayName = user.DisplayName;
             await ctx.Channel.SendMessageAsync(displayName).ConfigureAwait(false);
+        }
+
+        [Command("ping")]
+        [Description("Gets Kaitlyn's Ping! :D")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.Channel.SendMessageAsync($"Pong! <3\n*{ctx.Client.Ping}ms*").ConfigureAwait(false);
         }
     }
 }
