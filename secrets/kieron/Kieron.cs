76c5abe025c7f175dd3d9e7972da89371c10cd1d
Jay M <fruitsyberries@gmail.com>
2021-10-31 10:46:04 -0400

    Kieron, KIERON, KERIONNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN secret

---
+++ secrets/kieron/Kieron.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaitlyn.secrets.kieron
+{
+    class Kieron
+    {
+        static void Kiery(string[] args)
+        {
+            Console.WriteLine("Kieron was here. B)");
+        }
+    }
+}
