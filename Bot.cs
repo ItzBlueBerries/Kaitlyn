ac53116542fbfe2c939c010a4cb0d9e6dd51194a
fruit <fruit@DESKTOP-KC775Q6>
2021-10-31 07:34:58 -0400

    Add project files.

---
+++ Bot.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Kaitlyn.cmds;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaitlyn
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug,
+            };
+
+            Client = new DiscordClient(config);
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableMentionPrefix = true,
+                EnableDms = false,
+            };
+
+            Client.Ready += Client_Ready;
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            // Command Registration
+            Commands.RegisterCommands<BasicCmds>();
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ cmds/BasicCmds.cs
@@ -0,0 +1,43 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaitlyn.cmds
+{
+    public class BasicCmds : BaseCommandModule
+    {
+
+        // Hello Command
+        [Command("hello")]
+        [Description("Kaitlyn says Hi! <3")]
+        public async Task Hello(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Hello there! I'm Kaitlyn. <3\nYou can find my commands using `k!help`.").ConfigureAwait(false);
+        }
+
+        // Add Command
+        [Command("add")]
+        [Description("Adds two numbers together.")]
+        public async Task Add(CommandContext ctx, 
+            [Description("Num. 1")] int one, 
+            [Description("Num. 2")] int two)
+        {
+            await ctx.Channel.SendMessageAsync((one + two)
+                .ToString())
+                .ConfigureAwait(false);
+        }
+
+        // GetName Command
+        [Command("getname")]
+        [Description("Get a users name!")]
+        public async Task Getname(CommandContext ctx, [Description("Member Input")] DiscordMember user)
+        {
+            string displayName = user.DisplayName;
+            await ctx.Channel.SendMessageAsync(displayName).ConfigureAwait(false);
+        }
+    }
+}
